2192633f5222b502e5531693782a551622f993c1
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-06 19:08:43 -0300

    Preencher caracteristicas base da classe Fatura

--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -1,4 +1,42 @@
 package br.com.zup;
 
+import java.util.Date;
+
 public class Fatura {
+    private Consumidor consumidor;
+    private double valor;
+    private Date vencimento;
+
+    public Fatura(Consumidor consumidor, double valor, Date vencimento) {
+        this.consumidor = consumidor;
+        this.valor = valor;
+        this.vencimento = vencimento;
+    }
+
+    public Fatura() {
+    }
+
+    public Consumidor getConsumidor() {
+        return consumidor;
+    }
+
+    public void setConsumidor(Consumidor consumidor) {
+        this.consumidor = consumidor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Date vencimento) {
+        this.vencimento = vencimento;
+    }
 }
