2a2901a758b303c107bbd2329b3b0cf36320b35a
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-06 19:05:16 -0300

    Preencher caracteristicas base da classe Consumidor

--- src/br/com/zup/Consumidor.java
+++ src/br/com/zup/Consumidor.java
@@ -1,4 +1,20 @@
 package br.com.zup;
 
 public class Consumidor {
+    private String email;
+
+    public Consumidor(String email) {
+        this.email = email;
+    }
+
+    public Consumidor() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
